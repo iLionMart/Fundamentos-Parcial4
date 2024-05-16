30e11488e1f8fbae099ccbb52b54082de979dbfb
Lion Mart <45979134+iLionMart@users.noreply.github.com>
2024-05-16 08:17:26 -0600

    Create 12-UnidadArray2Class.java

---
+++ 12-UnidadArray2Class.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class main {
+    public static void main(String[] args) {
+
+        String tecladolim = JOptionPane.showInputDialog("Introduce la cantidad de numeros a entregar");
+        int limitearray = Integer.valueOf(tecladolim);
+
+        int[] numaleatorios = new int[limitearray];
+        int[] unidadnum = new int[limitearray];
+
+        String tecladolim2 = JOptionPane.showInputDialog("Introduzca la unidad que tiene que usar");
+        int unidadarray = Integer.valueOf(tecladolim2);
+
+        System.out.print("Los numeros con limite " + unidadarray + " son: ");
+        System.out.println();
+
+        for(int i=1; i < limitearray; i++){
+            numaleatorios[i] = (int) (Math.random() * (30 - 1) + 1);
+            unidadnum[i] = numaleatorios[i] % 10;
+            if(unidadnum[i] - unidadarray == 0){
+                System.out.print(numaleatorios[i] + " ");
+            }
+
+        }
+    }
+}
