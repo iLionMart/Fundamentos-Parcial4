8001152db24c9caa919d3056c4aa696c67bd88e9
Lion Mart <45979134+iLionMart@users.noreply.github.com>
2024-05-25 18:31:56 -0600

    Update 20-Proyecto.java

--- 20-Proyecto.java
+++ 20-Proyecto.java
@@ -1,13 +1,18 @@
 import javax.swing.*;
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class main {
     public static void main(String[] args) {
         int menu = 0;
-
-        File file = new File("C:\\Users\\ALUMNO\\IdeaProjects\\FundamentosProgramacion\\src\\diccionario.txt");
+        String contra = null;
+        String strHash;
+        String confirmador = "false";
+        byte[] msg;
+        byte[] hash;
+        StringBuilder strBuilder;
 
         while (menu != 3) {
             String menuh = JOptionPane.showInputDialog("Que operacion quieres realizar: 1) Codificar tu contraseña | 2) Desencriptar contraseña | 3) Cerrar");
@@ -15,10 +20,9 @@ public class main {
 
             switch (menu) {
                 case 1:
-                    String contra = JOptionPane.showInputDialog("Introduzca tu contraseña mi compa");
-
-                    byte[] msg = contra.getBytes();
-                    byte[] hash = null;
+                    contra = JOptionPane.showInputDialog("Introduzca tu contraseña mi compa");
+                    msg = contra.getBytes();
+                    hash = null;
 
                     try {
                         MessageDigest md = MessageDigest.getInstance("MD5");
@@ -26,18 +30,58 @@ public class main {
                     } catch (NoSuchAlgorithmException e) {
                         e.printStackTrace();
                     }
-                    StringBuilder strBuilder = new StringBuilder();
+
+                    strBuilder = new StringBuilder();
+    
                     for (byte b : hash) {
                         strBuilder.append(String.format("%02x", b));
                     }
-                    String strHash = strBuilder.toString();
-                    System.out.println("The MD5 hash: " + strHash);
+    
+                    strHash = strBuilder.toString();
+                    System.out.println("La contraseña encriptada en HASH " + strHash);
                     break;
 
                 case 2:
-                    String encriptado = JOptionPane.showInputDialog("Introduzca la encriptacion y se encontrara la contraseña");
+                    try {
+                        String encriptado = JOptionPane.showInputDialog("Introduzca la encriptacion y se encontrara la contraseña");
+                        FileReader file = new FileReader("C:\\Users\\leo10\\OneDrive\\Documentos\\Code\\Uni\\diccionario.txt");
+                        
+                        if(file.ready()){
+                            BufferedReader lector = new BufferedReader(file);
+                            while(confirmador == "false"){
+                                contra = lector.readLine();
+                                msg = contra.getBytes();
+                                hash = null;
 
-                    break;
+                                try {
+                                    MessageDigest md = MessageDigest.getInstance("MD5");
+                                    hash = md.digest(msg);
+                                } catch (NoSuchAlgorithmException e) {
+                                e.printStackTrace();
+                                }
+
+                                strBuilder = new StringBuilder();
+
+                                for (byte b : hash) {
+                                    strBuilder.append(String.format("%02x", b));
+                                }
+
+                                strHash = strBuilder.toString();
+                                System.out.println(contra);
+                                System.out.println(strHash);
+
+                                if (encriptado == strHash){
+                                    confirmador = "true";
+                                }
+                            }
+                                System.out.println("Del Hash: " + encriptado);
+                                System.out.println("La contraseña es: " + contra);
+                        }else{
+                            System.out.println("El archivo aun no esta cargado");
+                        }
+                    } catch (Exception e){
+                    System.out.println("Error: " + e.getMessage());
+                }
             }
         }
     }
